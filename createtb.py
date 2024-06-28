fac203437b7d127de508a4a470763f5d74d8b6ff
vajrabilash <degapoguvajrabilash@gmail.com>
2024-06-28 11:53:38 +0530

    creating table in database

--- createtb.py
+++ createtb.py
@@ -2,5 +2,8 @@
 import mysql.connector as mc  #importing required modules
 mydb = mc.connect(host='localhost',user='root',password='Jesusabhi@0399', db='db01') #connecting to the database and server
 mycursor = mydb.cursor()
-mycursor.execute("CREATE TABLE tbA(Name VARCHAR(20), Age VARCHAR(15))")  #query to create table in database
+#mycursor.execute("CREATE TABLE tbA(Name VARCHAR(20), Age VARCHAR(15))")  #query to create table in database
+mycursor.execute("SHOW TABLES")
+for x in mycursor:
+	print(x)
 #program ends here
