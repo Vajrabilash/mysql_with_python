057571d5e3b85dfcc7bdb5aa56ad0eb4c011289c
vajrabilash <degapoguvajrabilash@gmail.com>
2024-06-28 09:51:58 +0530

    connection to database

--- connection.py
+++ connection.py
@@ -1,8 +1,8 @@
-import mysql.connector as mc
-mydb=mc.connect(host='localhost', user='root', password='Jesusabhi@0399')
-mycursor=mydb.cursor()
-#mycursor.execute("CREATE DATABASE db01")
-if mydb.is_connected():
+import mysql.connector as mc # importing requied modules
+mydb=mc.connect(host='localhost', user='root', password='Jesusabhi@0399') # connecting to server
+mycursor=mydb.cursor() #cursor creation
+#mycursor.execute("CREATE DATABASE db01")  # creating database named as db01
+if mydb.is_connected():   
 	print("Connection established")
-else:
+else:  										#checking the connection weither connected or not
 	print("Failed to connect to database")
